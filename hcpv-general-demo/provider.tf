11758d2b6c982431d9258847bffec5265b6828e7
dgabrielov <109279263+dgabrielov@users.noreply.github.com>
2023-07-27 09:37:20 -0500

    Update provider.tf

--- hcpv-general-demo/provider.tf
+++ hcpv-general-demo/provider.tf
@@ -14,8 +14,8 @@ provider "aws" {
 provider "tfe" {}
 
 provider "vault" {
-  address = data.tfe_outputs.tfc-outputs.values.hcp-vault-address
-  token   = data.tfe_outputs.tfc-outputs.values.vault-token
+  address = 'https://vault-cluster-2-public-vault-ac568036.1e42f22e.z1.hashicorp.cloud:8200'
+  token   = 'hvs.CAESIG0lFSEm-nC4V6qSCwnmXHfMRKExU5iEsnhUCyJX1wqmGigKImh2cy5nOVpsb0dqMzE0WFUwTkZSbXhTRkgwQ3AudGNTTXEQvZBJ'
 }
 
 provider "hcp" {

95b0859a254a461aa67bf9b1cd3604f35fffe785
dgabrielov <109279263+dgabrielov@users.noreply.github.com>
2023-07-27 11:36:32 -0500

    Update provider.tf

--- hcpv-general-demo/provider.tf
+++ hcpv-general-demo/provider.tf
@@ -19,5 +19,7 @@ provider "vault" {
 }
 
 provider "hcp" {
+  client_id = "5ibO8BXZOJgR0pRzOUk4jw5x433oQyud"
+  client_secret = "n_7uWx5ShNAf0PQvAsLO4DA091o5_7ibc8W96jzonxt5wSTYsbYlA3dCq9Wu4KOp"
   project_id = "c96b78cf-a493-41ab-b708-96e89253c1b4"
 }
